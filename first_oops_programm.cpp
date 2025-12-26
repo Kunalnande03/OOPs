03598e965ad6a5bf6a60e8e308d5fbe39161bbcc
Kunal nande <nandekunal03@gmail.com>
2025-12-26 19:24:08 +0530

    Add files via upload

---
+++ first_oops_programm.cpp
@@ -0,0 +1,28 @@
+#include<stdio.h>
+using namespace std;
+class student
+{
+public:
+     int rollno;
+     char name[12];
+};
+int main()
+{
+   student s1,s2;
+   cout<<"\n Roll no ";
+   cin>>s1.rollno;
+   cout<<"\n Name ";
+   cin>>s1.name;
+   
+   cout<<"\n Roll no ";
+   cin>>s2.rollno;
+   cout>>"\n Name ";
+   cin>>s2.name:
+
+   cout<<"\n Roll no "<<s1.rollno;
+   cout<<"\n Name "<<s1.name;
+ 
+   cout<<"\n Roll no "<<s2.rollno;
+   cout<<"\n Name "<<s2.name;
+
+}
---
+++ first_prograam_c++.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+using namespace std;
+int main()
+{
+   int a,b,c;
+   cout<<"\nEnter first no ";
+   cin>>a;
+   cout<<"\nEnter second no ";
+   cin>>b;
+   c=a+b;
+   cout<<"\n Sum is "<< c;
+   cout<<"\n Sum of "<<a<<"and "<<b<<"is "<<c;
+}
